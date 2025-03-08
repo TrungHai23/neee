0d50275535f6baeb443325107eff2cb9e3d35063
TrungHai23 <haipt28082003@gmail.com>
2025-03-08 21:25:35 +0700

    2

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 onload = () => {
+    const text = `8/3 vui vẻ nhe :))) 💐`;
+    const container = document.getElementById("text-container");
+  
+    text.split("").forEach((char, index) => {
+        let span = document.createElement("span");
+        span.textContent = char;
+        span.classList.add("letter");
+        span.style.animationDelay = `${index * 0.1}s`; // Delay mỗi chữ một chút
+        container.appendChild(span);
+    });
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
   };
+
+  
